422d99f618f42af7195d48b83e9d45764886be85
KhadejaAhmedKZ <KhadejaAhmedKZ@users.noreply.github.com>
2026-07-02 10:47:42 +0000

    deploy: e0b646a2b9141bc46b79e84cc035245638349b6a

--- assets/useLocalStorage-DvXThaJw.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-CK1TLtc2.js";function u(t,e){const[r,c]=o.useState(()=>{try{const a=localStorage.getItem(t);return a?JSON.parse(a):e}catch{return e}});return o.useEffect(()=>{try{localStorage.setItem(t,JSON.stringify(r))}catch{}},[t,r]),[r,c]}export{u};
